5f95221d92566254bed8e14e4e82fb308a95c935
Jessica Sommerville <jessica.c.sommerville@gmail.com>
2023-04-14 13:55:17 -0700

    Completes lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solution here
+function findMatching(driverNames, nameToMatch){
+    return driverNames.filter((el) => el.toLowerCase()=== nameToMatch.toLowerCase());
+}
+
+function fuzzyMatch(driverNames, query){
+// count provided letters in the query and use that to get the piece at the beginning in order to match 
+    return driverNames.filter((el) => el.toLowerCase().slice(0, query.length) === query.toLowerCase());
+}
+
+function matchName(drivers, query){
+    return drivers.filter((driverObj) => driverObj.name.toLowerCase() === query.toLowerCase());
+}
